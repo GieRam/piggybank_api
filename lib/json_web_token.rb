21ad60fc75905017a24c1efaf8f683568dd012ea
giedrius-ramasauskas <giedrius.ramasauskas@vinted.com>
2019-03-10 12:11:58 +0200

    Lower jwt expires at value

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class JsonWebToken
-  def self.encode(payload, exp = 24.hours.from_now)
+  def self.encode(payload, exp = 2.hours.from_now)
     payload[:iat] = Time.zone.now.to_i
     payload[:exp] = exp.to_i
     JWT.encode(payload, Rails.application.secrets.secret_key_base)
